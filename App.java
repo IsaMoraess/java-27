d332e49cfc87d8edfe3554281efde9f82834ddf0
IsaMoraess <135661414+IsaMoraess@users.noreply.github.com>
2024-04-05 10:35:57 -0300

    Add files via upload

---
+++ App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int x; 
+        x = 150;
+        int y = 4; 
+        double resultado = x / y; // se divida um int por int msm o resultado sendo double
+        System.out.println(resultado); // a resposta ia ser 37.5 a parte decimal e ignorada
+    }
+}
